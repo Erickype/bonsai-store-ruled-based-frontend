dc210d8be2b343cee5044b8c37ecfb0f28821a23
Erickype <erickype@hotmail.com>
2023-06-04 14:31:07 -0500

    Logo in the navbar

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,27 @@
 <script>
     import "../app.css";
+    import logo from "$lib/assets/logo.svg"
   </script>
   
+<nav 
+class="
+mx-auto 
+p-4 
+bg-secondary-400">
+  <div class="container
+  mx-auto
+  flex
+  items-center
+  justify-between">
+    <a href="/" aria-label="Go to home page">
+      <img src={logo} 
+      width="200"
+      alt="Uptime logo"
+      class="w-10
+      md:w-15
+      lg:w-18">
+    </a>
+  </div>
+</nav>
+
   <slot />
