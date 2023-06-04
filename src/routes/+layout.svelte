7bfd65ebd094401ead526dd5c114e6ba7540b8bc
Erickype <erickype@hotmail.com>
2023-06-04 17:56:05 -0500

    Focus state and disposition of logo

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -13,13 +13,25 @@ bg-secondary-400">
   flex
   items-center
   justify-between">
-    <a href="/" aria-label="Go to home page">
-      <img src={logo} 
-      width="200"
-      alt="Uptime logo"
-      class="w-10
-      md:w-15
-      lg:w-18">
+    <a href="/" aria-label="Go to home page"
+    class="
+    focus:outline-none
+    focus-visible:ring-2
+    ring-gray-900
+    rounded-sm
+    ring-offset-4
+    ring-offset-secondary-400
+    lg:absolute
+    lg:left-1/2
+    lg:-translate-x-1/2
+    lg:top-9
+    z-50">
+    <img src={logo}
+    width="200"
+    alt="Uptime logo" 
+    class="w-10
+    md:w-15
+    lg:w-18">
     </a>
   </div>
 </nav>
