536cbd76b2427630ab9596b385391de048885138
Erickype <erickype@hotmail.com>
2023-06-05 13:26:40 -0500

    Header in +page.svelte

---
+++ src/routes/+page.svelte
@@ -0,0 +1,35 @@
+<script lang="ts">
+    import computer from "$lib/assets/computer.svg";
+</script>
+
+<header class="relative pt-16 md:pt-24 lg:pt-48">
+    <div
+      class="
+    absolute
+    inset-0
+    bottom-8 md:bottom-24 xl:bottom-32
+    -z-10
+    bg-gradient-to-b from-secondary-400 to-secondary-600
+    dark:from-secondary-800 dark:to-secondary-600"
+    />
+    <div
+      class="container
+    mx-auto
+    grid grid-rows-1
+    place-items-end
+    px-2"
+    >
+      <img
+        src={computer}
+        class="
+        row-start-1 row-end-2
+        col-start-1 col-end-2
+        mx-auto
+        w-40
+        sm:w-52
+        md:w-64
+        lg:w-72"
+        alt="Links"
+      />
+    </div>
+  </header>
