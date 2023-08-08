7d3d23cd75b977480cdafbe912d50e416021f9f9
Erick Carrasco <erickype@hotmail.com>
2023-08-08 20:11:12 +0000

    App bar todo functionality

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -7,20 +7,20 @@
 
   import { AppBar } from "@skeletonlabs/skeleton";
 
-  import { IconMenu } from '@tabler/icons-svelte';
+  import { IconMenu } from "@tabler/icons-svelte";
+  import { IconUser } from "@tabler/icons-svelte";
 
-  import type { LayoutData } from "./$types";
-
-  export let data: LayoutData;
-
-  let length = data.menuItems.length;
-  let title = "Bonsai Store"
+  let title = "Bonsai Store";
 </script>
 
-<AppBar gridColumns="grid-cols-3" slotDefault="place-self-center" slotTrail="place-content-end">
-	<svelte:fragment slot="lead"><IconMenu /></svelte:fragment>
-	<h1 class="text-xl">{title}</h1> 
-	<svelte:fragment slot="trail">(actions)</svelte:fragment>
+<AppBar
+  gridColumns="grid-cols-3"
+  slotDefault="place-self-center"
+  slotTrail="place-content-end"
+>
+  <svelte:fragment slot="lead"><IconMenu/></svelte:fragment>
+  <h1 class="text-xl">{title}</h1>
+  <svelte:fragment slot="trail"><IconUser/></svelte:fragment>
 </AppBar>
 
 <slot />
