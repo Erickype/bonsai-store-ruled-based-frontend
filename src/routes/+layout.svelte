fef6f7a59503349b29ab18d1f3a7e2c0be363d2b
Erickype <erickype@hotmail.com>
2023-08-08 14:41:55 -0500

    tabler-icons-svelte

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -7,6 +7,8 @@
 
   import { AppBar } from "@skeletonlabs/skeleton";
 
+  import { IconMenu } from '@tabler/icons-svelte';
+
   import type { LayoutData } from "./$types";
 
   export let data: LayoutData;
@@ -16,8 +18,8 @@
 </script>
 
 <AppBar gridColumns="grid-cols-3" slotDefault="place-self-center" slotTrail="place-content-end">
-	<svelte:fragment slot="lead">(icon)</svelte:fragment>
-	(title)
+	<svelte:fragment slot="lead"><IconMenu /></svelte:fragment>
+	<h1 class="text-xl">{title}</h1> 
 	<svelte:fragment slot="trail">(actions)</svelte:fragment>
 </AppBar>
 
