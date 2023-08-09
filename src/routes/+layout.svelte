b7825c6cda4ce35452446eba09d33bf46c0db8b9
Erick Carrasco <erickype@hotmail.com>
2023-08-09 23:33:46 +0000

    close the drawer on selection

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,4 @@
 <script lang="ts">
-  import "../app.postcss";
-
   import "@skeletonlabs/skeleton/themes/theme-skeleton.css";
   import "@skeletonlabs/skeleton/styles/skeleton.css";
   import "../app.postcss";
