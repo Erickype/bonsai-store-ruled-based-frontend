bb34d78c0d491d5085b1d11f292d2c1b3e89ccb7
Erick Carrasco <erickype@hotmail.com>
2023-08-12 00:35:43 +0000

    Highligth of current selected page

--- src/lib/navigation/navigation.svelte
+++ src/lib/navigation/navigation.svelte
@@ -1,5 +1,8 @@
 <script lang="ts">
+    import { page } from "$app/stores";
 	import { drawerStore } from "@skeletonlabs/skeleton";
+
+	$: classesActive = (href: string) => (href === $page.url.pathname ? '!bg-secondary-500' : '');
 
 	function drawerClose(): void {
 		drawerStore.close();
@@ -8,10 +11,10 @@
 
 <nav class="list-nav p-4">
 	<ul>
-		<li><a href="/" on:click={drawerClose}>Homepage</a></li>
-		<li><a href="/rules/editor" on:click={drawerClose}>Rules Editor</a></li>
-		<li><a href="/about" on:click={drawerClose}>About</a></li>
-		<li><a href="/blog" on:click={drawerClose}>Blog</a></li>
-		<li><a href="/contact" on:click={drawerClose}>Contact</a></li>
+		<li><a class="{classesActive("/")}" href="/" on:click={drawerClose}>Homepage</a></li>
+		<li><a class="{classesActive("/rules/editor")}" href="/rules/editor" on:click={drawerClose}>Rules Editor</a></li>
+		<li><a class="{classesActive("/about")}" href="/about" on:click={drawerClose}>About</a></li>
+		<li><a class="{classesActive("/blog")}" href="/blog" on:click={drawerClose}>Blog</a></li>
+		<li><a class="{classesActive("/contact")}" href="/contact" on:click={drawerClose}>Contact</a></li>
 	</ul>
 </nav>
