146af9903b9ac3b0a308a897fc20dfa0d9f582da
Erick Carrasco <erickype@hotmail.com>
2023-08-12 00:43:47 +0000

    Automation of menu nav items

--- src/lib/navigation/navigation.svelte
+++ src/lib/navigation/navigation.svelte
@@ -1,8 +1,22 @@
 <script lang="ts">
-    import { page } from "$app/stores";
+	import { page } from "$app/stores";
 	import { drawerStore } from "@skeletonlabs/skeleton";
 
-	$: classesActive = (href: string) => (href === $page.url.pathname ? '!bg-secondary-500' : '');
+	interface MenuItem {
+		name: string;
+		href: string;
+	}
+
+	const menuItems: MenuItem[] = [
+		{ name: "Homepage", href: "/" },
+		{ name: "Rules Editor", href: "/rules/editor" },
+		{ name: "About", href: "/about" },
+		{ name: "Blog", href: "/blog" },
+		{ name: "Contact", href: "/contact" },
+	];
+
+	$: classesActive = (href: string) =>
+		href === $page.url.pathname ? "!bg-primary-500" : "";
 
 	function drawerClose(): void {
 		drawerStore.close();
@@ -11,10 +25,16 @@
 
 <nav class="list-nav p-4">
 	<ul>
-		<li><a class="{classesActive("/")}" href="/" on:click={drawerClose}>Homepage</a></li>
-		<li><a class="{classesActive("/rules/editor")}" href="/rules/editor" on:click={drawerClose}>Rules Editor</a></li>
-		<li><a class="{classesActive("/about")}" href="/about" on:click={drawerClose}>About</a></li>
-		<li><a class="{classesActive("/blog")}" href="/blog" on:click={drawerClose}>Blog</a></li>
-		<li><a class="{classesActive("/contact")}" href="/contact" on:click={drawerClose}>Contact</a></li>
+		{#each menuItems as menu}
+			<li>
+				<a
+					class={classesActive(menu.href)}
+					href={menu.href}
+					on:click={drawerClose}
+				>
+					{menu.name}
+				</a>
+			</li>
+		{/each}
 	</ul>
 </nav>
