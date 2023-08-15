60feb56b28744a48140ba9b0864c24861ff0bfb5
Erick Carrasco <erickype@hotmail.com>
2023-08-15 17:28:44 +0000

    Endpoint to bet a rule by its ID

--- src/lib/server/services/rules/endpoints.ts
+++ src/lib/server/services/rules/endpoints.ts
@@ -1,8 +1,14 @@
 import { domain } from "$lib/server/domain";
-import type { GetResponse } from "./rule";
+import type { GetResponse, Rule } from "./rule";
 
 export const Get = async () => {
     const res = await fetch(domain + `/rule`);
     const data = await res.json() as GetResponse;
     return data.Rules
+}
+
+export const GetById = async (id:string) => {
+    const res = await fetch(domain + `/rule/`+id)
+    const data = await res.json() as Rule
+    return data
 }
