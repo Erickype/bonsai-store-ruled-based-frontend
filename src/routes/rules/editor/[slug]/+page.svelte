a53028af2524d40587686686827ff967ca2b80d0
Erick Carrasco <erickype@hotmail.com>
2023-08-15 18:00:03 +0000

    Preview od rule ID to test

---
+++ src/routes/rules/editor/[slug]/+page.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+    import type { PageServerData } from "./$types";
+
+    export let data: PageServerData
+    const {rule} = data
+    
+</script>
+
+<pre>
+    {rule.id}
+</pre>
