8b7eecca170cb7cfa37fcd6b288f2c135b49f5b2
Erick Carrasco <erickype@hotmail.com>
2023-08-16 20:09:25 +0000

    basic form to update a rule

--- src/routes/rules/editor/[slug]/+page.svelte
+++ src/routes/rules/editor/[slug]/+page.svelte
@@ -1,11 +1,79 @@
 <script lang="ts">
+    import { enhance } from "$app/forms";
     import type { PageServerData } from "./$types";
 
-    export let data: PageServerData
-    const {rule} = data
-    
+    export let data: PageServerData;
+    const { rule } = data;
 </script>
 
-<pre>
-    {rule.id}
-</pre>
+<form method="POST" action="?/updateRule" use:enhance>
+    <label class="label">
+        <span>ID</span>
+        <input
+            class="input"
+            type="text"
+            placeholder="Input"
+            disabled
+            value={rule.id}
+            name="id"
+        />
+    </label>
+
+    <label class="label">
+        <span>Name</span>
+        <input
+            class="input"
+            type="text"
+            placeholder="Input"
+            value={rule.name}
+            name="name"
+        />
+    </label>
+
+    <label class="label">
+        <span>Description</span>
+        <input
+            class="input"
+            type="text"
+            placeholder="Input"
+            value={rule.desc}
+            name="desc"
+        />
+    </label>
+
+    <label class="label">
+        <span>Salience</span>
+        <input
+            class="input"
+            type="number"
+            placeholder="Input"
+            value={rule.salience}
+            min="0"
+            name="salience"
+        />
+    </label>
+
+    <label class="label">
+        <span>When</span>
+        <input
+            class="input"
+            type="text"
+            placeholder="Input"
+            value={rule.when}
+            name="when"
+        />
+    </label>
+
+    <label class="label">
+        <span>Then</span>
+        <input
+            class="input"
+            type="text"
+            placeholder="Input"
+            value={rule.then}
+            name="then"
+        />
+    </label>
+
+    <button type="submit" class="btn variant-filled">Update</button>
+</form>
