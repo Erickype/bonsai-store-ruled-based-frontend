19897dea5dccb3cbc4deec9a4d02c8ec4fb593d7
Erickype <erickype@hotmail.com>
2023-08-28 13:56:21 -0500

    Form then and when of rule fields component

---
+++ src/lib/forms/ruleEditor/ruleFields.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+    export let when: any;
+    export let then: any;
+</script>
+
+<div class="columns-2">
+    <label class="label">
+        <span>When</span>
+        <input
+            class="input"
+            type="text"
+            placeholder="Input"
+            value={when}
+            name="when"
+        />
+    </label>
+
+    <label class="label">
+        <span>Then</span>
+        <input
+            class="input"
+            type="text"
+            placeholder="Input"
+            value={then}
+            name="then"
+        />
+    </label>
+</div>
