868787aee4ab194560ce2bbf8237d8f29f37163f
Erickype <erickype@hotmail.com>
2023-08-31 14:37:40 -0500

    Preview of the when field

--- src/lib/forms/ruleEditor/ruleFields.svelte
+++ src/lib/forms/ruleEditor/ruleFields.svelte
@@ -1,19 +1,28 @@
 <script lang="ts">
-    export let when: any;
-    export let then: any;
+    import type { PairExpression } from "$lib/server/services/rules/rule";
+
+    export let when: { [key: string]: { [key: string]: PairExpression[] }[] };
+    export let then: string[];
+
 </script>
 
 <div class="columns-2">
-    <label class="label">
-        <span>When</span>
-        <input
-            class="input"
-            type="text"
-            placeholder="Input"
-            value={when}
-            name="when"
-        />
-    </label>
+    <span>When</span>
+    {#each Object.entries(when) as [condition, compoundConditions]}
+        <p>{condition}</p>
+        {#each compoundConditions as compoundCondition}
+            {#each Object.entries(compoundCondition) as [operator, expressions]}
+                <p>{operator}</p>
+                {#each expressions as expression}
+                    {#if expression.const == undefined}
+                        <p>{expression.obj}</p>
+                    {:else}
+                        <p>{expression.const}</p>
+                    {/if}
+                {/each}
+            {/each}
+        {/each}
+    {/each}
 
     <label class="label">
         <span>Then</span>
