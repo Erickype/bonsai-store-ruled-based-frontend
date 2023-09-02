bd017eff51780a96ecde40e83072d1885275fd8a
Erick Carrasco <erickype@hotmail.com>
2023-09-02 16:24:34 +0000

    Select of conditions component

---
+++ src/lib/forms/ruleEditor/conditionsSelect.svelte
@@ -0,0 +1,34 @@
+<script lang="ts">
+    const conditions: string[] = [
+        "and",
+        "or",
+        "eq",
+        "not",
+        "gt",
+        "gte",
+        "lt",
+        "lte",
+        "bor",
+        "band",
+    ];
+
+    export let selected: string;
+
+    $:{selected}
+
+    function UpdateSelected(e: Event) {
+        const newValue = (e.target as HTMLSelectElement).value;
+        selected = newValue;
+    }
+</script>
+
+<select
+    class="select"
+    value={selected}
+    name={selected}
+    on:change={UpdateSelected}
+>
+    {#each conditions as condition}
+        <option value={condition}>{condition.toLocaleUpperCase()}</option>
+    {/each}
+</select>
