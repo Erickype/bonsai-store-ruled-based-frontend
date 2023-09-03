dc11a3eae1a7f99162b3bda32efea2ac99b843e6
Erick Carrasco <erickype@hotmail.com>
2023-09-03 20:52:16 +0000

    Use of components

--- src/lib/forms/ruleEditor/ruleFields.svelte
+++ src/lib/forms/ruleEditor/ruleFields.svelte
@@ -1,7 +1,8 @@
 <script lang="ts">
     import type { PairExpression } from "$lib/server/services/rules/rule";
 
-    import ConditionsSelect from "$lib/forms/ruleEditor/conditionsSelect.svelte";
+    import When from "$lib/forms/ruleEditor/when.svelte";
+    import Then from "$lib/forms/ruleEditor/then.svelte";
 
     export let when: { [key: string]: { [key: string]: PairExpression[] }[] };
     export let then: string[];
@@ -9,52 +10,9 @@
 
 <div class="flex-1 lg:flex lg:flex-row">
     <div class="w-full lg:basis-1/2 lg:pr-2">   
-        <span>When</span>
-        <div class="card">
-            {#each Object.entries(when) as [condition, compoundConditions]}
-                <section class="p-4">
-                    <ConditionsSelect selected={condition} />
-                    {#each compoundConditions as compoundCondition}
-                        {#each Object.entries(compoundCondition) as [operator, expressions]}
-                            <section class="p-4">
-                                <ConditionsSelect selected={operator} />
-                                {#each expressions as expression}
-                                <section class="p-2">
-
-                                    {#if expression.const == undefined}
-                                        <input
-                                            class="input"
-                                            type="text"
-                                            value={expression.obj}
-                                            name="obj"
-                                            />
-                                            {:else}
-                                            <input
-                                            class="input"
-                                            type="text"
-                                            value={expression.const}
-                                            name="const"
-                                        />
-                                        {/if}
-                                    </section>
-                                        {/each}
-                            </section>
-                        {/each}
-                    {/each}
-                </section>
-            {/each}
-        </div>
+        <When when={when}/>
     </div>
     <div class="w-full lg:basis-1/2 lg:pl-2">
-        <label class="label">
-            <span>Then</span>
-            <input
-                class="input"
-                type="text"
-                placeholder="Input"
-                value={then}
-                name="then"
-            />
-        </label>
+        <Then then={then}/>
     </div>
 </div>
