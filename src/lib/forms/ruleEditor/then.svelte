69e3da9906b27f8a9b29e9fbd305d459a777977e
Erick Carrasco <erickype@hotmail.com>
2023-09-03 20:52:05 +0000

    When and then components

---
+++ src/lib/forms/ruleEditor/then.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+    export let then: string[];
+</script>
+
+<label class="label">
+    <span>Then</span>
+    <input
+        class="input"
+        type="text"
+        placeholder="Input"
+        value={then}
+        name="then"
+    />
+</label>
---
+++ src/lib/forms/ruleEditor/when.svelte
@@ -0,0 +1,41 @@
+<script lang="ts">
+    import type { PairExpression } from "$lib/server/services/rules/rule";
+    import ConditionsSelect from "./conditionsSelect.svelte";
+
+    export let when: { [key: string]: { [key: string]: PairExpression[] }[] };
+</script>
+
+<span>When</span>
+<div class="card">
+    {#each Object.entries(when) as [condition, compoundConditions]}
+        <section class="p-4">
+            <ConditionsSelect selected={condition} />
+            {#each compoundConditions as compoundCondition}
+                {#each Object.entries(compoundCondition) as [operator, expressions]}
+                    <section class="p-4">
+                        <ConditionsSelect selected={operator} />
+                        {#each expressions as expression}
+                            <section class="p-2">
+                                {#if expression.const == undefined}
+                                    <input
+                                        class="input"
+                                        type="text"
+                                        value={expression.obj}
+                                        name="obj"
+                                    />
+                                {:else}
+                                    <input
+                                        class="input"
+                                        type="text"
+                                        value={expression.const}
+                                        name="const"
+                                    />
+                                {/if}
+                            </section>
+                        {/each}
+                    </section>
+                {/each}
+            {/each}
+        </section>
+    {/each}
+</div>
