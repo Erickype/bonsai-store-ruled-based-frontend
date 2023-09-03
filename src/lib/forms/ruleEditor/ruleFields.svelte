e6301b504d68eefb84c6bfe5d5ec77f2242b4b0e
Erick Carrasco <erickype@hotmail.com>
2023-09-03 21:30:44 +0000

    Modifying to use When field from rule

--- src/lib/forms/ruleEditor/ruleFields.svelte
+++ src/lib/forms/ruleEditor/ruleFields.svelte
@@ -1,18 +1,19 @@
 <script lang="ts">
     import type { PairExpression } from "$lib/server/services/rules/rule";
+    import type { Then } from "$lib/server/services/rules/rule";
 
     import When from "$lib/forms/ruleEditor/when.svelte";
-    import Then from "$lib/forms/ruleEditor/then.svelte";
+    import ThenC from "$lib/forms/ruleEditor/then.svelte";
 
     export let when: { [key: string]: { [key: string]: PairExpression[] }[] };
-    export let then: string[];
+    export let then: Then[];
 </script>
 
 <div class="flex-1 lg:flex lg:flex-row">
-    <div class="w-full lg:basis-1/2 lg:pr-2">   
-        <When when={when}/>
+    <div class="w-full lg:basis-1/2 lg:pr-2">
+        <When {when} />
     </div>
     <div class="w-full lg:basis-1/2 lg:pl-2">
-        <Then then={then}/>
+        <ThenC {then} />
     </div>
 </div>
