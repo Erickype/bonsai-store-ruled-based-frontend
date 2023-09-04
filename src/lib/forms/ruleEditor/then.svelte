0972c3c36f80a38b2c0b895cdb4264eb7ce96a82
Erick Carrasco <erickype@hotmail.com>
2023-09-04 18:44:24 +0000

    Set action objects

--- src/lib/forms/ruleEditor/then.svelte
+++ src/lib/forms/ruleEditor/then.svelte
@@ -1,9 +1,12 @@
 <script lang="ts">
-    import type { Set, Then } from "$lib/server/services/rules/rule";
+    import type {
+        PairExpression,
+        Set,
+        Then,
+    } from "$lib/server/services/rules/rule";
     import ConditionsSelect from "./conditionsSelect.svelte";
 
     export let then: Then[];
-    console.log(then);
 
     function getSelected(set?: Set): string {
         if (set) {
@@ -15,25 +18,59 @@
         }
         return "";
     }
-    console.log(getSelected(then.at(0)?.set?.at(1)));
+    function getSelectedObj(set?: Set): PairExpression[] {
+        if (set) {
+            if (set.div) return set.div;
+            if (set.minus) return set.minus;
+            if (set.mod) return set.mod;
+            if (set.mul) return set.mul;
+            if (set.plus) return set.plus;
+        }
+        return [];
+    }
 </script>
 
 <span>Then</span>
 <div class="card">
     <section class="p-4">
-        <label for="">Set</label>
+        <h4 class="h4">Set</h4>
         {#each then as action}
             {#if action.call === undefined}
-                <div class="grid gap-4 grid-cols-2 grid-rows-1 pb-2">
-                    <input
-                        class="input"
-                        type="text"
-                        placeholder="Input"
-                        value={action.set?.at(0)?.obj}
-                    />
-                    <ConditionsSelect
-                        selected={getSelected(action.set?.at(1))}
-                    />
+                <div class="card mb-2">
+                    <section class="p-4">
+                        <div class="grid gap-4 grid-cols-2 grid-rows-1 pb-2">
+                            <input
+                                class="input"
+                                type="text"
+                                placeholder="Input"
+                                value={action.set?.at(0)?.obj}
+                            />
+                            <ConditionsSelect
+                                selected={getSelected(action.set?.at(1))}
+                            />
+                        </div>
+                    </section>
+                    <section class="p-4">
+                        <div class="grid gap-4 grid-cols-2 grid-rows-1 pb-2">
+                            {#each getSelectedObj(action.set?.at(1)) as expression}
+                                {#if expression.const == undefined}
+                                    <input
+                                        class="input"
+                                        type="text"
+                                        value={expression.obj}
+                                        name="obj"
+                                    />
+                                {:else}
+                                    <input
+                                        class="input"
+                                        type="text"
+                                        value={expression.const}
+                                        name="const"
+                                    />
+                                {/if}
+                            {/each}
+                        </div>
+                    </section>
                 </div>
             {/if}
         {/each}
