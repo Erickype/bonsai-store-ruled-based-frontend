98781452b0f033f35f7b8862cef60944babc1834
Erick Carrasco <erickype@hotmail.com>
2023-09-04 17:04:25 +0000

    Call action shown

--- src/lib/forms/ruleEditor/then.svelte
+++ src/lib/forms/ruleEditor/then.svelte
@@ -1,14 +1,47 @@
 <script lang="ts">
-    export let then: string[];
+    import type { Then } from "$lib/server/services/rules/rule";
+
+    export let then: Then[];
+    console.log(then);
 </script>
 
-<label class="label">
-    <span>Then</span>
-    <input
-        class="input"
-        type="text"
-        placeholder="Input"
-        value={then}
-        name="then"
-    />
-</label>
+<span>Then</span>
+<div class="card">
+    <section class="p-4">
+        <label for="">Set</label>
+        {#each then as action}
+            {#if action.call === undefined}
+                <pre>
+                    {action.set?.at(0)?.obj}
+                </pre>
+            {/if}
+        {/each}
+    </section>
+    <section class="p-4">
+        <h4 class="h4">Call</h4>
+        <div class="grid gap-4 grid-cols-2 grid-rows-1">
+            {#each then as action}
+                {#if action.set === undefined}
+                    <label class="label">
+                        <span>Command</span>
+                        <input
+                            class="input"
+                            type="text"
+                            placeholder="Input"
+                            value={action.call?.at(0)}
+                        />
+                    </label>
+                    <label class="label">
+                        <span>Constant</span>
+                        <input
+                            class="input"
+                            type="text"
+                            placeholder="Input"
+                            value={action.call?.at(1).const}
+                        />
+                    </label>
+                {/if}
+            {/each}
+        </div>
+    </section>
+</div>
