43994a21d9e1b12aae64f6ef2336420a02c24c2f
Erick Carrasco <erickype@hotmail.com>
2023-09-07 21:16:04 +0000

    Top navigation

---
+++ src/lib/navigation/topNav.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+    import { TabGroup, TabAnchor } from "@skeletonlabs/skeleton";
+
+    import { page } from "$app/stores";
+
+    const menuItems: MenuItem[] = [
+        { name: "Homepage", href: "/" },
+        { name: "About", href: "/about" },
+        { name: "Features", href: "/features" },
+        { name: "Products", href: "/products" },
+        { name: "Contact", href: "/contact" },
+    ];
+</script>
+
+<TabGroup
+    justify="justify-center"
+    active="variant-filled-primary"
+    hover="hover:variant-soft-primary"
+    flex="flex-1 lg:flex-none"
+    rounded="rounded-lg"
+    class="bg-surface-100-800-token w-full hidden lg:block md:block"
+>
+    {#each menuItems as menu}
+        <TabAnchor href={menu.href} selected={$page.url.pathname === menu.href}>
+            <span>{menu.name}</span>
+        </TabAnchor>
+    {/each}
+</TabGroup>
