25f791141afaaf038c2eac514bc54e9a7f417931
Erick Carrasco <erickype@hotmail.com>
2023-09-14 17:43:24 +0000

    .env variables using svelte syntax

--- src/lib/firebase/firebase.client.ts
+++ src/lib/firebase/firebase.client.ts
@@ -1,29 +1,32 @@
-// Import the functions you need from the SDKs you need
-import { deleteApp, getApp, getApps, initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import {
+  PUBLIC_APIKEY,
+  PUBLIC_AUTHDOMAIN,
+  PUBLIC_PROJECTID,
+  PUBLIC_STORAGEBUCKET,
+  PUBLIC_MESSAGINGSENDERID,
+  PUBLIC_APPID
+} from "$env/static/public"
 
-import { getAuth, setPersistence, inMemoryPersistence } from "firebase/auth";
+import { getApps, initializeApp } from "firebase/app";
+import { getAuth} from "firebase/auth";
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_APIKEY,
-  authDomain: import.meta.env.VITE_AUTHDOMAIN,
-  projectId: import.meta.env.VITE_PROJECTID,
-  storageBucket: import.meta.env.VITE_STORAGEBUCKET,
-  messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
-  appId: import.meta.env.VITE_APPID
+  apiKey: PUBLIC_APIKEY,
+  authDomain: PUBLIC_AUTHDOMAIN,
+  projectId: PUBLIC_PROJECTID,
+  storageBucket: PUBLIC_STORAGEBUCKET,
+  messagingSenderId: PUBLIC_MESSAGINGSENDERID,
+  appId: PUBLIC_APPID
 };
 
 // Initialize Firebase
-let firebaseApp
-
+let firebaseApp;
+ 
 if (!getApps().length) {
-  firebaseApp = initializeApp(firebaseConfig)
-} else {
-  firebaseApp = getApp()
-  deleteApp(firebaseApp)
-  firebaseApp = initializeApp(firebaseConfig)
+  firebaseApp = initializeApp(firebaseConfig);
 }
-
-export const auth = getAuth(firebaseApp)
+ 
+// Auth
+const firebaseAuth = getAuth(firebaseApp);
+ 
+export { firebaseApp, firebaseAuth };
