1ece3d9c5fe34f7db24f268b3cfec292f434b277
Erick Carrasco <erickype@hotmail.com>
2023-09-14 20:52:32 +0000

    Manage state for login and signup

--- src/lib/components/auth/email.svelte
+++ src/lib/components/auth/email.svelte
@@ -1,17 +1,16 @@
 <script lang="ts">
-    let register = true
-    let email: string
-    let password: string
-    let confirmPassword: string
+    let register = true;
+    let email: string;
+    let password: string;
+    let confirmPassword: string;
 </script>
 
-<form
-    class="flex content-center items-center h-full p-10">
+<form class="flex content-center items-center h-full p-10">
     <div
-        class="card h-2/3 lg:h-full p-10 mx-auto flex flex-col item place-content-between gap-4 lg:w-1/3
+        class="card h-min p-10 mx-auto flex flex-col item place-content-between gap-4 lg:w-1/3
         variant-filled-tertiary"
     >
-        <h2 class="h2 text-center">Sign Up</h2>
+        <h2 class="h2 text-center">{register ? "Sign Up" : "Log In"}</h2>
         <label class="label">
             <span>Email</span>
             <input
@@ -35,20 +34,30 @@
         </label>
 
         {#if register}
-        <label class="label">
-            <span>Confirm password</span>
-            <input
-                class="input"
-                type="password"
-                placeholder="Confirm password"
-                required
-                bind:value={confirmPassword}
-            />
-        </label>
+            <label class="label">
+                <span>Confirm password</span>
+                <input
+                    class="input"
+                    type="password"
+                    placeholder="Confirm password"
+                    required
+                    bind:value={confirmPassword}
+                />
+            </label>
         {/if}
 
+        <div class="text-center mx-auto">
+            {#if register}
+                <p>Already have an account?</p>
+                <p>Log In</p>
+            {:else}
+                <p>Don't have an account?</p>
+                <p>Sign Up</p>
+            {/if}
+        </div>
+
         <button type="submit" class="btn variant-filled-success mx-auto"
-            >Sign Up</button
+            >Submit</button
         >
     </div>
 </form>
