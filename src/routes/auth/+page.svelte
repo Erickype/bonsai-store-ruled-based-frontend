ead6d20ee3f5584580bb7102841bf98429ad4f08
Erick Carrasco <erickype@hotmail.com>
2023-09-16 19:34:38 +0000

    Show off error

--- src/routes/auth/+page.svelte
+++ src/routes/auth/+page.svelte
@@ -1,5 +1,13 @@
 <script lang="ts">
     import EmailAuth from "$lib/components/auth/email.svelte";
+    import type { ActionData } from "./$types";
+
+    export let form: ActionData;
 </script>
 
-<EmailAuth/>
+<div class="flex flex-col flex-1 items-center">
+    {#if form?.differentPassword}<p class="error">Passwords don't match!</p>{/if}
+    {#if form?.firebaseError}<p class="error">{form?.authErrorCode}</p>{/if}
+
+    <EmailAuth />
+</div>
