af1b4f15b0ed911be6286f35ff2becf9f22dab06
Erick Carrasco <erickype@hotmail.com>
2023-09-18 17:35:58 +0000

    Auth moved to client

--- src/lib/components/auth/email.svelte
+++ src/lib/components/auth/email.svelte
@@ -1,18 +1,79 @@
 <script lang="ts">
-    import { enhance } from "$app/forms";
+    import type { AuthError } from "firebase/auth";
+    import { authHandler } from "../../../stores/authStore";
 
     let register = true;
     let email: string;
     let password: string;
     let confirmPassword: string;
+
+    let error: string;
+
+    async function submit() {
+        console.log(register, email, password, confirmPassword);
+        if (register) {
+            return SignUp();
+        } else {
+            return LogIn();
+        }
+    }
+
+    async function LogIn() {
+        if (!email) {
+            error = "Email required!!";
+            return;
+        }
+        if (!password) {
+            error = "Password required!!";
+        }
+        try {
+            await authHandler.login(email, password);
+        } catch (err) {
+            const authError: AuthError = err as AuthError;
+            error = getFormattedErrorCode(authError.code);
+            return;
+        }
+    }
+
+    async function SignUp() {
+        if (!email) {
+            error = "Email required!!";
+            return;
+        }
+        if (!password || !confirmPassword) {
+            error = "Passwords required!!";
+            return;
+        }
+        if (!(password === confirmPassword)) {
+            error = "Passwords don't match!!";
+            return;
+        }
+        try {
+            await authHandler.signUp(email, password);
+        } catch (err) {
+            const authError: AuthError = err as AuthError;
+            error = getFormattedErrorCode(authError.code);
+            return;
+        }
+    }
+
+    function getFormattedErrorCode(code: string) {
+        code = code.replace("auth/", "");
+        code = code.replace(/-/g, " ");
+        code = code.charAt(0).toUpperCase() + code.slice(1);
+        return code;
+    }
 </script>
 
-<form
-    class="flex content-center items-center h-full p-10"
-    method="POST"
-    action="?/submit"
-    use:enhance
->
+<div class="text-center pt-4">
+    {#if error}
+        <span class="chip variant-filled-error">
+            <p class="error">{error}</p>
+        </span>
+    {/if}
+</div>
+
+<form class="flex content-center items-center h-full p-10">
     <div
         class="card h-min p-10 mx-auto flex flex-col item place-content-between gap-4 lg:w-1/3
         variant-filled-tertiary"
@@ -57,8 +118,10 @@
             </label>
         {/if}
 
-        <button type="submit" class="btn variant-filled-success mx-auto"
-            >Submit</button
+        <button
+            type="submit"
+            class="btn variant-filled-success mx-auto"
+            on:click={submit}>Submit</button
         >
 
         <div class="text-center mx-auto">
--- src/routes/auth/+page.svelte
+++ src/routes/auth/+page.svelte
@@ -1,23 +1,7 @@
 <script lang="ts">
     import EmailAuth from "$lib/components/auth/email.svelte";
-    import type { ActionData } from "./$types";
-
-    export let form: ActionData;
 </script>
 
 <div class="flex flex-col">
-    <div class="text-center pt-4">
-        {#if form?.differentPassword}
-            <span class="chip variant-filled-error">
-                <p class="error">Passwords don't match!</p>
-            </span>
-        {/if}
-        {#if form?.firebaseError}
-            <span class="chip variant-filled-error">
-                <p class="error">{form?.authErrorCode}</p>
-            </span>
-        {/if}
-    </div>
-
-    <EmailAuth/>
+    <EmailAuth />
 </div>
