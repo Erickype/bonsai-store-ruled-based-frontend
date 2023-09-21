89821ab81bbf7f7e39ebfe4e7a55ed291b0eb833
Erick Carrasco <erickype@hotmail.com>
2023-09-21 18:07:56 +0000

    User dashboard and account page

---
+++ src/routes/user/account/+page.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+    import { authUserStore } from "../../../stores/authStore";
+
+    let email: string;
+    authUserStore.subscribe((curr) => {
+        email = curr.currentUser?.email as string;
+    });
+</script>
+
+<pre>
+    {email}
+</pre>
